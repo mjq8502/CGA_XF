dd76e43d3c881ba313a364e66086ad0c959844c1
mjq8502 <mjquinn8502@gmail.com>
2017-07-15 19:31:17 -0500

    Initial Commit

---
+++ GolfApp2/GolfApp2/GolfApp2/Main.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GolfApp2
+{
+    public partial class Main : ContentPage
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            this.mainTeesButton.Clicked += async (sender, args) =>
+            {
+                await Navigation.PushModalAsync(new Tees());
+            };
+        }
+
+        //Hey you there
+
+    }
+}
---
+++ GolfApp2/GolfApp2/GolfApp2/Tees.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GolfApp2
+{
+    public partial class Tees : ContentPage
+    {
+        public Tees()
+        {
+            InitializeComponent();
+        }
+    }
+}
